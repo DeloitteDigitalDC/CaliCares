a71ce9a4c9066eae084961e4912e4ce5759c0d22
Siddarth Sivakumar <sisivakumar@deloitte.com>
2016-05-24 16:23:19 -0400

    Added angular-google-map api and placed map on home page

--- client/app/views/home/home.controller.js
+++ client/app/views/home/home.controller.js
@@ -14,7 +14,22 @@
     .module('rex')
     .controller('HomeCtrl', HomeCtrl);
 
-  function HomeCtrl() {
+  function HomeCtrl($scope) {
+    var vm = this;
+    vm.map = {
+      center: {
+        latitude: 37.79,
+        longitude: -122.4175
+      },
+      zoom: 13
+    };
+    vm.marker = {
+      idKey: 123,
+      coords: {
+        latitude: 37.7836377,
+        longitude: -122.4132168
+      }
+    };
   }
 
 })();
