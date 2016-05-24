fc1f30f97c4f6072fd846809ad383ee69aa64a1b
Rajendran, Rohit (US - Arlington) <rorajendran@deloitte.com>
2016-05-24 17:22:55 -0400

    Fixed route for facilities controller

--- server/router.js
+++ server/router.js
@@ -12,6 +12,7 @@
 var config = require('./config');
 
 module.exports = function router(app, auth) {
+  app.use('/facilities', require('./api/facilities/'));
   app.use('/fda', require('./api/fda/')(auth));
   app.use('/user', require('./api/user/')(auth));
 
