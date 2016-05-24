6584f85d249f3ecc0d9447fa694b3a9cddd008db
Siddarth Sivakumar <sisivakumar@deloitte.com>
2016-05-24 18:07:18 -0400

    Working on geocoder to get coords from address

--- client/app/views/home/home.controller.js
+++ client/app/views/home/home.controller.js
@@ -18,17 +18,41 @@
     var vm = this;
     vm.map = {
       center: {
-        latitude: 37.79,
-        longitude: -122.4175
+        latitude: 0,
+        longitude: 0
       },
       zoom: 13
     };
-    vm.marker = {
-      idKey: 123,
-      coords: {
-        latitude: 37.7836377,
-        longitude: -122.4132168
-      }
+    init();
+
+    function createMap() {
+      console.log('Lat:' + vm.lat);
+      vm.map.center.latitude = vm.lat;
+      vm.map.center.longitude = vm.lng;
+      vm.markers = [
+        {
+          id: "1",
+          markerCoords: {
+              latitude: vm.lat,
+              longitude: vm.lng
+          }
+        }
+      ];
+    }
+
+
+    function init(){
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode( { "address": "1919 N Lynn Street Rosslyn, VA" }, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+            var location = results[0].geometry.location,
+            lat = location.lat(),
+            lng = location.lng();
+            vm.lat = lat;
+            vm.lng = lng;
+            createMap();
+          }
+      });
     };
   }
 
