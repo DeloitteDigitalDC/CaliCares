4ddc95552ce350893e05accd4d6d0b841433fdf6
Rajendran, Rohit (US - Arlington) <rorajendran@deloitte.com>
2016-05-26 00:27:24 -0400

    Displays message in inbox view

--- client/app/views/inbox/inbox.controller.js
+++ client/app/views/inbox/inbox.controller.js
@@ -5,19 +5,51 @@
  *
  * @name Inbox
  *
- * @description
+ * @text
  * Controller for rex
  */
 (function () {
 
   angular
     .module('rex')
-    .controller('Inbox', Inbox);
+    .controller('InboxCtrl', Inbox);
 
   function Inbox() {
     var vm = this;
-
+    
     vm.foo = [];
+    
+    vm.msgs = [
+      {
+        child: "Timmy Smith",
+        dateCreated: "05/19/2016",
+        eventType: "Emotional outburst",
+        eventNo: "1234-15184",
+        caseworker: "James Smith",
+        description: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.",
+        comments:[
+          {
+            commentor: "James Smith",
+            time: "7:15am",
+            date: "May 13th",
+            text: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla"
+          },
+          {
+            commentor: "Chuck Taylor",
+            time: "1:10pm",
+            date: "May 16th",
+            text: "Etiam porta sem malesuada magna mollis euismod."
+          },
+          {
+            commentor: "James Smith",
+            time: "3:13pm",
+            date: "May 14th",
+            text: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla"
+          }
+        ]
+      }
+    ];
+   
   }
 
 })();
