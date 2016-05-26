785dff1844f57f24177cb2aebdfcd2222770e6b7
Rajendran, Rohit (US - Arlington) <rorajendran@deloitte.com>
2016-05-26 15:13:00 -0400

    Added message list

--- client/app/views/inbox/inbox.controller.js
+++ client/app/views/inbox/inbox.controller.js
@@ -16,35 +16,94 @@
 
   function Inbox() {
     var vm = this;
+    vm.selected = 0;
     
-    vm.foo = [];
+    vm.selectMsg = function(num){
+      vm.selected = num;
+    };
     
     vm.msgs = [
       {
-        child: "Timmy Smith",
-        dateCreated: "05/19/2016",
-        eventType: "Emotional outburst",
-        eventNo: "1234-15184",
-        caseworker: "James Smith",
-        description: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.",
+        child: 'Timmy Smith',
+        dateCreated: '05/19/2016',
+        eventType: 'Emotional outburst',
+        eventNo: '1234-15184',
+        caseworker: 'James Smith',
+        description: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.',
         comments:[
           {
-            commentor: "James Smith",
-            time: "7:15am",
-            date: "May 13th",
-            text: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla"
+            commentor: 'James Smith',
+            time: '7:15am',
+            date: 'May 13th',
+            text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla'
           },
           {
-            commentor: "Chuck Taylor",
-            time: "1:10pm",
-            date: "May 16th",
-            text: "Etiam porta sem malesuada magna mollis euismod."
+            commentor: 'Chuck Taylor',
+            time: '1:10pm',
+            date: 'May 16th',
+            text: 'Etiam porta sem malesuada magna mollis euismod.'
           },
           {
-            commentor: "James Smith",
-            time: "3:13pm",
-            date: "May 14th",
-            text: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla"
+            commentor: 'James Smith',
+            time: '3:13pm',
+            date: 'May 14th',
+            text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla'
+          }
+        ]
+      },
+      {
+        child: 'Timmy Smith2',
+        dateCreated: '05/19/2016',
+        eventType: 'Emotional outburst',
+        eventNo: '1234-15184',
+        caseworker: 'James Smith',
+        description: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.',
+        comments:[
+          {
+            commentor: 'James Smith',
+            time: '7:15am',
+            date: 'May 13th',
+            text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla'
+          },
+          {
+            commentor: 'Chuck Taylor',
+            time: '1:10pm',
+            date: 'May 16th',
+            text: 'Etiam porta sem malesuada magna mollis euismod.'
+          },
+          {
+            commentor: 'James Smith',
+            time: '3:13pm',
+            date: 'May 14th',
+            text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla'
+          }
+        ]
+      },
+      {
+        child: 'Timmy Smith3',
+        dateCreated: '05/19/2016',
+        eventType: 'Emotional outburst',
+        eventNo: '1234-15184',
+        caseworker: 'James Smith',
+        description: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.',
+        comments:[
+          {
+            commentor: 'James Smith',
+            time: '7:15am',
+            date: 'May 13th',
+            text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla'
+          },
+          {
+            commentor: 'Chuck Taylor',
+            time: '1:10pm',
+            date: 'May 16th',
+            text: 'Etiam porta sem malesuada magna mollis euismod.'
+          },
+          {
+            commentor: 'James Smith',
+            time: '3:13pm',
+            date: 'May 14th',
+            text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla'
           }
         ]
       }
