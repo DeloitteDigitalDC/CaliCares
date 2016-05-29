20cff8ed9f96c2f582300d5ea0f62bb6c2ab0dd4
Rajendran, Rohit (US - Arlington) <rorajendran@deloitte.com>
2016-05-29 19:22:10 -0400

    Added functionality behind pressing send when replying to a message

--- client/app/components/inbox-msg/inbox-msg.directive.js
+++ client/app/components/inbox-msg/inbox-msg.directive.js
@@ -10,7 +10,7 @@
  *  
  * @property {Array} messages - a specific message object
  *
- * @example <inbox-msg messages="msgs"></inbox-msg>
+ * @example <inbox-msg messages='msgs'></inbox-msg>
  */
 (function () {
 
@@ -20,28 +20,41 @@
 
   function inboxMsg() {
     return {
-        restrict: 'E',
-        templateUrl: 'app/components/inbox-msg/inbox-msg.directive.html',
-        scope: {
-          messages: '=',
-          num: '='
-        },
-        link: link
+      restrict: 'E',
+      templateUrl: 'app/components/inbox-msg/inbox-msg.directive.html',
+      scope: {
+        messages: '=',
+        num: '='
+      },
+      link: link
     };
 
     function link(scope) {
       scope.showHistory = false;
       scope.showReply = false;
-      
-      scope.toggleHistory = function(){
+      scope.replyTxt = 'test';
+      scope.toggleHistory = function () {
         scope.showHistory = !scope.showHistory;
-      }
-      scope.toggleReply = function(){
+      };
+      scope.toggleReply = function () {
         scope.showReply = !scope.showReply;
-      }
-      scope.replyMsg = function(reply){
+      };
+      scope.replyMsg = function () {
+        scope.messages[scope.num].previousMessages.push({
+            from: scope.messages[scope.num].from,
+            time: scope.messages[scope.num].time,
+            date: scope.messages[scope.num].dateCreated,
+            message: scope.messages[scope.num].message
+        });
+
+        var date = new Date();
+        scope.messages[scope.num].time = date.getTime();
+        scope.messages[scope.num].dateCreated = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
+        scope.messages[scope.num].message = scope.replyTxt;
         
-      }
+        scope.replyTxt = '';
+        scope.showReply = false;
+      };
     }
   }
 
