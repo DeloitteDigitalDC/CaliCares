43df6ebcef16c55858bbed519d06124b01c61772
Rajendran, Rohit (US - Arlington) <rorajendran@deloitte.com>
2016-05-31 13:04:50 -0400

    Properly show time for replied messages

--- client/app/components/inbox-msg/inbox-msg.directive.js
+++ client/app/components/inbox-msg/inbox-msg.directive.js
@@ -47,13 +47,24 @@
         });
 
         var date = new Date();
-        scope.messages[scope.num].time = date.getTime();
+        scope.messages[scope.num].time = formatAMPM(date);
         scope.messages[scope.num].dateCreated = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
         scope.messages[scope.num].message = scope.replyTxt;
         
         scope.replyTxt = '';
         scope.showReply = false;
       };
+    }
+    
+    function formatAMPM(date) {
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0'+minutes : minutes;
+      var strTime = hours + ':' + minutes + ' ' + ampm;
+      return strTime;
     }
   }
 
