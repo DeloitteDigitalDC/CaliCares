7e459a1bb8c5c0511dab4b7b5b66cf83a365a24b
Rajendran, Rohit (US - Arlington) <rorajendran@deloitte.com>
2016-05-31 13:15:50 -0400

    Replied messages now show correct time and user

--- client/app/components/inbox-msg/inbox-msg.directive.js
+++ client/app/components/inbox-msg/inbox-msg.directive.js
@@ -18,7 +18,7 @@
     .module('rex')
     .directive('inboxMsg', inboxMsg);
 
-  function inboxMsg() {
+  function inboxMsg(user) {
     return {
       restrict: 'E',
       templateUrl: 'app/components/inbox-msg/inbox-msg.directive.html',
@@ -35,34 +35,41 @@
       scope.toggleHistory = function () {
         scope.showHistory = !scope.showHistory;
       };
+
       scope.toggleReply = function () {
         scope.showReply = !scope.showReply;
       };
+
       scope.replyMsg = function () {
         scope.messages[scope.num].previousMessages.push({
-            from: scope.messages[scope.num].from,
-            time: scope.messages[scope.num].time,
-            date: scope.messages[scope.num].dateCreated,
-            message: scope.messages[scope.num].message
+          from: scope.messages[scope.num].from,
+          time: scope.messages[scope.num].time,
+          date: scope.messages[scope.num].dateCreated,
+          message: scope.messages[scope.num].message
         });
 
         var date = new Date();
         scope.messages[scope.num].time = formatAMPM(date);
         scope.messages[scope.num].dateCreated = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
         scope.messages[scope.num].message = scope.replyTxt;
+
+        user.getDetails().then(function (userData) {
+          console.log(userData);
+          scope.messages[scope.num].from = userData.data.data.nickName;
+        });
         
         scope.replyTxt = '';
         scope.showReply = false;
       };
     }
-    
+
     function formatAMPM(date) {
       var hours = date.getHours();
       var minutes = date.getMinutes();
       var ampm = hours >= 12 ? 'pm' : 'am';
       hours = hours % 12;
       hours = hours ? hours : 12; // the hour '0' should be '12'
-      minutes = minutes < 10 ? '0'+minutes : minutes;
+      minutes = minutes < 10 ? '0' + minutes : minutes;
       var strTime = hours + ':' + minutes + ' ' + ampm;
       return strTime;
     }
